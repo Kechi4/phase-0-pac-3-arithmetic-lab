64ddcf3bcf0cd334796bb1ca1303f89f7de7ec72
Nkechi Ukome <n.ukome@yahoo.com>
2022-12-12 22:54:36 -0500

    commit tree 6c5d55e5266d80bdea7e566fcf2bf4e57ad99160

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+    // we'll fill this in shortly
+  }
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+function makeInt(n) {
+    return parseInt}
+
+function makeInt() {
+        return parseInt}
+
+makeInt("2");
+
+ function makeInt(a) {
+    return parseInt(a,10);
+
+ }
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
